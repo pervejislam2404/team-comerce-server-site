96aa8fcd53854491aec08b8db1b5ce01f2ffb5d6
pervejislam2404 <pervejislam2404@gmail.com>
2022-01-01 01:47:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(port, () => {
+    console.log('server running at port' + port);
+});
